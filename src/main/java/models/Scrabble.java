be6ba7dd0b6114cb03b64407f31c690382f38881
Abdullah Yahya and Danyel Murray <danyel@>
2018-01-02 11:39:00 -0800

    Cleaned up code

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -1,22 +1,7 @@
 package models;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.io.IOException;
-
 public class Scrabble {
-    private static String[] letterKeys = new String[] { "A", "B", "C", "D",
-                                                    "E", "F", "G", "H",
-                                                    "I", "J", "K", "L",
-                                                    "M", "N", "O", "P",
-                                                    "Q", "R", "S", "T",
-                                                    "U", "V", "W", "X",
-                                                    "Y", "Z" };
-
-
     public Integer calculateScore(String word) {
-        Map<String, Integer> lettersAndValues = new HashMap<>();
-
         char[] charArray = word.toUpperCase().toCharArray();
         int score = 0;
 
@@ -37,28 +22,6 @@ public class Scrabble {
                 score += 10;
             }
         }
-
-        // Pair Keys with their respective Values and put them in the HashMap
-//        for (String eachLetter : letterKeys) {
-//            if (eachLetter.equals("A") || eachLetter.equals ("E") || eachLetter.equals ("I") || eachLetter .equals ("O") || eachLetter .equals("U") || eachLetter .equals ("L") || eachLetter .equals ("N") || eachLetter .equals ("R") || eachLetter .equals ("S") || eachLetter .equals ("T"))  {
-//                lettersAndValues.put(eachLetter, 1);
-//            } else if (eachLetter .equals ("D") || eachLetter .equals ("G")) {
-//                lettersAndValues.put(eachLetter, 2);
-//            } else if (eachLetter .equals ("B") || eachLetter .equals ("C") || eachLetter .equals ("M") || eachLetter .equals ("P")) {
-//                lettersAndValues.put(eachLetter, 3);
-//            } else if (eachLetter .equals ("F") || eachLetter .equals ("H") || eachLetter .equals ("V") || eachLetter .equals ("W") || eachLetter .equals ("Y")) {
-//                lettersAndValues.put(eachLetter, 4);
-//            } else if (eachLetter .equals ("K")) {
-//                lettersAndValues.put(eachLetter, 5);
-//            } else if (eachLetter .equals ("J") || eachLetter .equals ("X")) {
-//                lettersAndValues.put(eachLetter, 8);
-//            } else if (eachLetter .equals ("Q") || eachLetter .equals ("Z")) {
-//                lettersAndValues.put(eachLetter, 10);
-//            }
-//        }
-
-        // Character.toUpperCase(letter) Capitalizes
         return score;
     }
-
 }
