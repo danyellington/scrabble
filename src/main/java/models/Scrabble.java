65080eddf563d29c8de893f8e333fa6bc73b41be
Abdullah Yahya and Danyel Murray <abdullah@>
2018-01-02 13:14:11 -0800

    Refactored to use HashMaps

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -1,27 +1,46 @@
 package models;
 
-public class Scrabble {
-    public Integer calculateScore(String word) {
-        char[] charArray = word.toUpperCase().toCharArray();
-        int score = 0;
+import java.util.Map;
+import java.util.HashMap;
 
-        for (char letter : charArray) {
-            if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'L' || letter == 'N' || letter == 'R' || letter == 'S' || letter == 'T') {
-                score += 1;
-            } else if (letter == 'D' || letter == 'G') {
-                score += 2;
-            } else if (letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P') {
-                score += 3;
-            } else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y') {
-                score += 4;
-            } else if (letter == 'K') {
-                score += 5;
-            } else if (letter == 'J' || letter == 'X') {
-                score += 8;
-            } else if (letter == 'Q' || letter == 'Z') {
-                score += 10;
+public class Scrabble {
+    private static Character[] letterKeys = new Character[] { 'A', 'B', 'C', 'D',
+                                                                'E', 'F', 'G', 'H',
+                                                                'I', 'J', 'K', 'L',
+                                                                'M', 'N', 'O', 'P',
+                                                                'Q', 'R', 'S', 'T',
+                                                                'U', 'V', 'W', 'X',
+                                                                'Y', 'Z' };
+
+    public Integer calculateScore(String word) {
+        Map<Character, Integer> wordsAndNumbers = new HashMap<Character, Integer>();
+        int score = 0;
+        char[] charArray = word.toUpperCase().toCharArray();
+
+        for (char eachLetter : letterKeys) {
+            if (eachLetter == 'A' || eachLetter == 'E' || eachLetter == 'I' || eachLetter == 'O' || eachLetter =='U' || eachLetter == 'L' || eachLetter == 'N' || eachLetter == 'R' || eachLetter == 'S' || eachLetter == 'T')  {
+                    wordsAndNumbers.put(eachLetter, 1);
+                } else if (eachLetter == 'D' || eachLetter == 'G') {
+                    wordsAndNumbers.put(eachLetter, 2);
+                } else if (eachLetter == 'B' || eachLetter == 'C' || eachLetter == 'M' || eachLetter == 'P') {
+                    wordsAndNumbers.put(eachLetter, 3);
+                } else if (eachLetter == 'F' || eachLetter == 'H' || eachLetter == 'V' || eachLetter == 'W' || eachLetter == 'Y') {
+                    wordsAndNumbers.put(eachLetter, 4);
+                } else if (eachLetter == 'K') {
+                    wordsAndNumbers.put(eachLetter, 5);
+                } else if (eachLetter == 'J' || eachLetter == 'X') {
+                    wordsAndNumbers.put(eachLetter, 8);
+                } else if (eachLetter == 'Q' || eachLetter == 'Z') {
+                    wordsAndNumbers.put(eachLetter, 10);
+                }
+        }
+
+        for (char eachChar : charArray) {
+            if (wordsAndNumbers.containsKey(eachChar)) {
+                score += wordsAndNumbers.get(eachChar);
             }
         }
+
         return score;
     }
 }
--- src/test/java/models/ScrabbleTest.java
+++ src/test/java/models/ScrabbleTest.java
@@ -10,8 +10,8 @@ public class ScrabbleTest {
     @Test
     public void calculateScore_returnsScoreForSingleLetter_1() throws Exception {
         Scrabble testScrabble = new Scrabble();
-        Integer expected = 10;
-        assertEquals(expected, testScrabble.calculateScore("z"));
+        Integer expected = 1;
+        assertEquals(expected, testScrabble.calculateScore("A"));
     }
 
     @Test
